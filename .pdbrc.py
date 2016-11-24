d17297327e4f9ab731501ce70ff510d521231699
Remco Haszing <remcohaszing@gmail.com>
2016-11-24 19:38:15 +0100

    Make pdb++ sticky

--- .pdbrc.py
+++ .pdbrc.py
@@ -1,5 +1,10 @@
 import pdb
 
+# Only use dependencies of pdbpp here.
+from pygments.console import colorize
+
 
 class Config(pdb.DefaultConfig):
+    #prompt = colorize('green', '(Pdb) ')
+    sticky_by_default = True
     current_line_color = 0
